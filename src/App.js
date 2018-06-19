8a0963f310a156a8978a7c470db77c8675e2bd0d
Nick-Hericks <nickhericks@gmail.com>
2018-06-19 15:39:46 +0200

    commit to nick branch before start over

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import scriptLoader from 'react-async-script-loader';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    map: null
+  }
+
+  componentWillReceiveProps({isScriptLoadSucceed}){
+    // Constructor creates a new map - only center and zoom are required.
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -25.958295, lng: 32.591222},
+      zoom: 14,
+    });
+
+    this.setState({
+      map: map
+    })
+
+  }
+
+
+
+
+
   render() {
     return (
       <div className="App">
+      
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Maputo Restaurants</h1>
+          <span></span>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="container">
+          <div className="side-bar">
+            <div>
+              <input id="zoom-to-area-text" type="text" placeholder="Filter restaurants by name"/>
+            </div>
+          </div>
+
+          <div id="map"></div>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default scriptLoader(
+    [`https://maps.googleapis.com/maps/api/js?v=3&libraries=places&key=AIzaSyB8u8SHJ767vZjqYAHapTSCLFDDwXVCt-E`]
+)(App);
